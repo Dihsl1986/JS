83c18d66fa7306389d7fddf353ccb4b8e627213d
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-22 03:19:22 -0300

    Sempre treina Github

---
+++ comentario.js
@@ -0,0 +1,13 @@
+//Todos os codigos que eu colocar sejam de comentários os códigos serão motivacionais
+
+console.log('Bem vindo ao inicio agora a parada ficou séria.')
+console.log('Olá Diego Treine Todos os dia aproveite a oportunidade.')
+
+//Aqui eu tenho um comentário em linha. 
+
+/* Para comentar mais de uma linha*/
+
+/* Aqui eu tenho comentários multi linhas
+ oque ajuda muito para a fixação do tema na mente.
+ "O único objetivo que eu tenho em mente é dar a minha filha uma boa vida.*/
+ console.log('não quero ficar rico quero ser melhor doque posso ser apanas superando a mim mesmo')  
---
+++ string.js
@@ -0,0 +1,11 @@
+/*
+
+Straings São cadeias de caracteres
+
+"" // aspas duplas
+'' // aspas simples
+`` // template literal ou template strins
+
+*/
+
+console.log("ma/'yk")

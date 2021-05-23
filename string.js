f978cc8343fecd344b53c4123668496784970d02
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-23 01:07:36 -0300

    String modificada pela 3º vez.

--- string.js
+++ string.js
@@ -4,10 +4,23 @@ Straings São cadeias de caracteres
 
 "" // aspas duplas
 '' // aspas simples
-`` // template literal ou template strins
-
+`` // Crase literal ou template strings são crase e permitem que eu use multi linhas
+      e também permite que eu coloque expressões de linguagens
 */
 
 console.log("ma/'yk")
 console.log("Diego Santos Lima")
+console.log('Valentina Santos Silva')
+console.log(`Simone da Silva Zacarias 
+ "Jose Evaristo de Lima"
+ ' Anthony Sem vergonha'`)
+
+ // Crase = `` permite que também coloque expressões de linguagens dentro da própria string. 
+// cha,amos essa possibilidade de colocar valores de interpolação.
+
+
+ console.log(`Soma ${2 + 2}`);
+ console.log(`Subtração ${2 - 1}`);
+ console.log(`Divisão ${2 / 2}`);
+ console.log(`Multiplicação ${2 * 2}`);
 

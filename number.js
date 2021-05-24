f51bf364d892ea61a34473e0d3c31f1b7e50b42b
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-24 01:04:30 -0300

    Aula de number

---
+++ number.js
@@ -0,0 +1,44 @@
+/* 
+Number 
+      * Números
+      33 // Inteiros
+      12.5 // Reais = Float
+      NaN // Not a Number = Não é um número
+      Infinity // Infinito
+ */
+
+// Numeros inteiros
+console.log(-33)
+console.log(33)
+console.log(5)
+
+// Números Reais
+console.log(12.5)
+console.log(123.75)
+console.log(25.5)
+console.log(1234.56)
+
+ // Exemplos
+ console.log(12.5 + 33)
+ console.log(123.75 / 5)
+ console.log(25.5 * 15)
+ console.log(1234.56 - 0.56)
+
+ // Vai dar erro
+ /* Se eu tento multiplicar um número por uma 
+    string por exemplo:
+    
+    console.log(12.5 / "asdf") o resultado será NaN = Não é um número
+*/
+
+// Infinito
+console.log(12.5 === Infinity)
+
+/* Neste caso ele apenas verifica se é verdadeiro ou falso 
+e responde pois 12.5 não é infinito. Ou seja infinity é apenas a palavra infinito mesmo. */
+console.log(Infinity)
+
+// vai dar erro 
+//infinity minúsculo vai dar um erro de referência.
+
+//console.log(infinity)

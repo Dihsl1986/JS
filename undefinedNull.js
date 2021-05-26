68928a5ced8cfa8da485a891f7dcb597dbd5f333
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-25 23:07:01 -0300

    Aula undefined e null

---
+++ undefinedNull.js
@@ -0,0 +1,17 @@
+/* 
+
+* undefined
+      * indefinido
+
+* null
+      * nulo
+      * objeto que não possui nada dentro
+      * diferente de indefinido
+
+
+*/ 
+
+console.log(undefined);
+console.log(null); // é diferente de indefinido
+
+console.log(null === undefined);

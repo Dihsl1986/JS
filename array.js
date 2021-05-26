8c8c849b3a3088abac3f4fa623a2337be0414298
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-26 01:01:35 -0300

    Aula de Array e conclusão

---
+++ array.js
@@ -0,0 +1,59 @@
+/*
+
+      * Array (Vetores)-> Um tipo de dado estrutural
+            * Uma lista
+            * Agrupamento
+            
+
+
+            ["Diego", 36]
+
+            Array é uma estrutura de dados diferente do object pois 
+            é uma seguida da outra.
+*/
+
+console.log([
+      "Leite",
+      "Ovos",
+      "Feijão",
+      2,
+      3,
+      5
+]);
+/*
+
+      # Tipos de dados
+            Conforme o ECMAScript standard temos 9 tipos de daos:
+            * Data types
+                  * Primitive / Primitive value
+                  * Structural
+                  * Structural Primitive
+                  
+                  ## Primitivos -> não é objeto e seu valor não vai sofrer mutação são constantes.
+
+                        * String 
+                        * Number
+                        * Boolean
+                        * Undefined
+                        * Symbol
+                        * BiInt
+                  
+                  ## Estruturais
+
+                        * Object -> Tem uma estrutura em sí.
+                           * Array
+                           * Map
+                           * Set
+                           * Date
+                           * ...
+                        
+                        * Function
+                  ## Primitivo Estrutural / Structural Root Primitive
+
+                        * Null
+                        * 
+                  
+
+
+
+*/

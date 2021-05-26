fb6f053f9baab1e5e74aafd491b0a7f42c8efded
Diego Santos Lima <dihsl1986@gmail.com>
2021-05-25 23:25:53 -0300

    Aula object e mudei o arquivo de script para object.js

---
+++ object.js
@@ -0,0 +1,23 @@
+/* 
+
+* Object -> Um tipo de dado que é chamado de estrutural
+      * Object = objeto
+      * Propriedade / Atributos
+      * Funcionalidades /Métodos
+      
+      "Todo objeto tem atributos e métodos"
+
+      { propriedade: "valor" }
+
+*/
+
+console.log({
+      name: "Diego", // Criei um objeto que tem propriedade de name e valor Diego
+      idade: 35,
+      andar: function() { //Funcionalidade andar
+            console.log('andar') // se é uma funcionalidade é um método pois esta dentro de um objeto.
+      }
+});
+
+ 
+

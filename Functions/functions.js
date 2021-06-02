90004dc450b78adb8f347935e2b6eaa89e2654e3
Diego Santos Lima <dihsl1986@gmail.com>
2021-06-02 02:28:45 -0300

    Atualizando recomeçando FUNCTION

---
+++ Functions/functions.js
@@ -0,0 +1,26 @@
+// Funções or functions
+// Também é um tipo de dados.
+
+
+//Este é o primeiro passo a declaração da função
+//function statement
+
+function createPhrases() { //<- o js guarda o codigo na memoria registrando a função uma única vez
+   console.log("Estudar é muito bom!!");
+   console.log("Paciência e persistência");
+   console.log("Revisão é a mãe do aprendizado."); 
+}
+
+//Para executar a função. rodar,chamar,invocar ou 
+//execute, run, call , invoke.
+// coloco apena o nome da função junto com (); isso será o suficiente 
+//para rodar.
+createPhrases(); // <- e aqui ele imprime oque esta dentro dessa função
+                  // <- finalizando o código.
+                  
+                  createPhrases(); 
+                  createPhrases(); 
+                  createPhrases(); 
+                  createPhrases(); 
+                  createPhrases(); 
+console.log("fim do programa.");

74474ceefc9107b8485c610066743f5242d7df7c
Diego Santos Lima <dihsl1986@gmail.com>
2021-06-18 00:46:18 -0300

    Atualizando aula de aporadores lógicos.

--- Functions/operadoresDeComparacao.js
+++ Functions/operadoresDeComparacao.js
@@ -6,12 +6,67 @@ let one = 1;
 let two = 2;
 
 // == igual a
-console.log(" == Igua a");
-console.log(two == 1);
-console.log(one == "1");
+//console.log(" == Igua a");
+//console.log(two == 1);
+
+
+//onsole.log(one == "1");
 
 // != diferente de
-console.log("!= diferente de"); 
-console.log(one != two);
-console.log(one != 1);
-console.log(one != "1");
+    //console.log("!= diferente de"); 
+    //console.log(one != two);
+    //console.log(one != 1);
+    //console.log(one != "1");
+
+// === estritamente igual a
+    //console.log(one === "1"); // igual no valor e diferente no tipo
+   // console.log(one === 1); // 
+
+   // !== estritamente difernte de
+        //console.log(two !== "2");// é diferente no seu tipo. 
+        //console.log(two !== 2);
+
+    //// > maior que
+        //console.log(one > two);
+    /// >= maior ou igual que 
+        //console.log(one >= 1);
+        //console.log(two >= 1);
+
+    /// < menor que
+        //console.log(one < two);
+
+    /// <= menor ou igua a
+        //console.log(one <= two);
+        //console.log(one <= 1);
+        //console.log(one <= 0);
+
+//////////// Operadores de atribuição  (Assigment)
+ let x
+
+ //console.log(x); // vai dar undefined
+
+ // assigment
+x = 1 // <= a variável x vai estar recebendo 1 recebe
+     
+ // addition assigment
+ // neste caso será x = x + 2 
+  x += 2
+  
+ // subtraction assigment
+ //neste caso será x = x - 1
+   x -= 1
+   
+ // multiplication assigment
+ //neste caso x = x * 2
+  x *= 2
+  
+
+ // division assigment
+ //neste caso  será com x = / 2
+ x /= 2
+
+ // remainder, exponetiation
+ // x %= 2
+
+ x ** 2
+ console.log(x); 

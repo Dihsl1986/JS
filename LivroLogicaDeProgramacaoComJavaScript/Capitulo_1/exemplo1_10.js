d0a8755ca0167c9b00217e5ca48315fe3a3f31e7
Diego Santos Lima <dihsl1986@gmail.com>
2021-08-15 02:28:40 -0300

    Exercicio 1.10

---
+++ LivroLogicaDeProgramacaoComJavaScript/Capitulo_1/exemplo1_10.js
@@ -0,0 +1,7 @@
+// lêr o dado de entrada.
+var dayNumbers = (3);
+var hoursNumber = (4);
+//Calcular a duração
+var totalHours = hoursNumber + (dayNumbers * 24);
+//Exibir o total
+console.log(`O total de horas: ${totalHours}`);

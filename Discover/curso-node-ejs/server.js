9834645dde582d1a345d60e34d20d8b62635d45e
Diego Santos Lima <dihsl1986@gmail.com>
2021-09-16 00:56:30 -0300

    Finalizada a aula de EJS

--- Discover/curso-node-ejs/server.js
+++ Discover/curso-node-ejs/server.js
@@ -4,7 +4,41 @@ const app = express ();
 app.set("view engine", "ejs");
 
 app.get("/", function (req,res){
-     res.render("pages/index");
+    const items = [
+        {
+            title: "D",
+            message: "esenvolver aplicações/Serviços de forma rápida e fácil."
+        },
+
+        { 
+            title: "E",
+            message: "JS usa JavaScript para renderizar HTML."
+        },
+        { 
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        { 
+            title: "A",
+            message: "morzinho!!"
+        },
+        { 
+            title: "I",
+            message: "nstall EJS"
+        },
+        { 
+            title: "S",
+            message: "intax simples!!"
+        }
+
+
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de página HTML"
+     res.render("pages/index", {
+         qualitys:items,
+         subtitle:subtitle,
+     });
  })
 
 app.get("/sobre", function (req,res){

bb9a679f09a326db26793ba004d01112025f9737
Diego Santos Lima <dihsl1986@gmail.com>
2021-10-25 23:40:18 -0300

    Conversor de Real para Dólar

---
+++ LivroLogicaDeProgramacaoComJavaScript/Capitulo_2/converterDolar.js
@@ -0,0 +1,8 @@
+function converter (reais) {
+
+    dolar = reais / 5.56;
+   
+}
+converter(1500000);
+
+console.log(("É esse o valorem dolar: "), (dolar));
--- LivroLogicaDeProgramacaoComJavaScript/Capitulo_2/js/resp2_cNode.js
+++ LivroLogicaDeProgramacaoComJavaScript/Capitulo_2/js/resp2_cNode.js
@@ -1,25 +1,18 @@
-  //Criar uma referencia ao elemnto da paginando
-    
-   
-   
-
-
-
+    // Os parametros da variável dentro do parametro.
     function calcularPreco(nomeProduto = "Produto",valor = precoProduto ) {
        
         Produto = nomeProduto;  // Vai receber o nome do produto
         precoProduto = valor; //Vai receber valor do produto
         Promocao = ((precoProduto * 2) + (precoProduto * 0.5)).toFixed(2); //Calcula a valorda promoção
-        precoDoProduto3 = (precoProduto * 0.5).toFixed(2);
+        precoDoProduto3 = (precoProduto * 0.5).toFixed(2); //Calcula o promoção do treceiro produto.
 
     }
 
     calcularPreco("Arroz",17.00);
-   
-    console.log(("Promoção:"),(Produto),("leve 3 por R$:"),(Promocao));
-   
-    console.log(("O 3º sai por R$:"),(precoDoProduto3));
-    console.log(("Preço da integral"),(precoProduto));
+    
+    console.log(("Promoção:"),(Produto),("leve 3 por R$:"),(Promocao)); //Vai imprimir o preço da promoção d produto.
+    console.log(("O 3º sai por R$:"),(precoDoProduto3)); ///Vai imprimir o valor do terceio remédio da promoção
+    console.log(("Preço da integral"),(precoProduto)); // Vai imprimiro preço integral do produto.
     
     
 
